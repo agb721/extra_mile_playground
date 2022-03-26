da4d7b7ea84637027ff99d5d0c12338d862742d8
The-Michael-Chen <02chenmichael@gmail.com>
2022-03-26 12:19:04 -0400

    added the app

---
+++ ExtraMileApp/FirebaseTest/app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.firebasetest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.ResolveInfo;
+import android.content.pm.ServiceInfo;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.util.ArrayUtils;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.neuos.INeuosSdk;
+import io.neuos.INeuosSdkListener;
+import io.neuos.NeuosSDK;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchPage2(View view) {
+        Intent intent = new Intent(this, BrainActivity.class);
+        startActivity(intent);
+    }
+
+}
